283da430d05c00a9126b0e1aba845125953f0d8b
andr3i1010 <andrei.k@polarnl.org>
2025-12-20 15:49:26 +0100

    disallow interactions

--- src/main/kotlin/org/polarnl/polarLobby/util/allowBlockBreak.kt
+++ src/main/kotlin/org/polarnl/polarLobby/util/allowBlockBreak.kt
@@ -1,9 +1,12 @@
 package org.polarnl.polarLobby.util
 
+import org.bukkit.Material
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.block.Action
 import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.event.block.BlockPlaceEvent
+import org.bukkit.event.player.PlayerInteractEvent
 import org.polarnl.polarLobby.PolarLobby
 import java.util.UUID
 
@@ -30,5 +33,41 @@ class AllowBlockBreak(private val plugin: PolarLobby) : Listener {
         }
         event.isCancelled = true
         PlayerMessage().send("<red><bold>You cannot place blocks in this server", event.player)
+    }
+
+    @EventHandler
+    fun onPlayerInteract(event: PlayerInteractEvent) {
+        if (event.action != Action.RIGHT_CLICK_BLOCK) return
+
+        val block = event.clickedBlock ?: return
+        val player = event.player
+
+        if (allowedPlayers.contains(player.uniqueId)) {
+            return
+        }
+
+        val blockedBlocks = setOf(
+            Material.CHEST, Material.TRAPPED_CHEST, Material.ENDER_CHEST,
+            Material.BARREL, Material.FURNACE, Material.BLAST_FURNACE,
+            Material.SMOKER, Material.HOPPER, Material.DROPPER,
+            Material.DISPENSER, Material.CRAFTING_TABLE, Material.ENCHANTING_TABLE,
+            Material.ANVIL, Material.CHIPPED_ANVIL, Material.DAMAGED_ANVIL,
+            Material.BREWING_STAND, Material.BEACON,
+            Material.OAK_DOOR, Material.SPRUCE_DOOR, Material.BIRCH_DOOR,
+            Material.JUNGLE_DOOR, Material.ACACIA_DOOR, Material.DARK_OAK_DOOR,
+            Material.CRIMSON_DOOR, Material.WARPED_DOOR, Material.IRON_DOOR,
+            Material.OAK_TRAPDOOR, Material.SPRUCE_TRAPDOOR, Material.BIRCH_TRAPDOOR,
+            Material.JUNGLE_TRAPDOOR, Material.ACACIA_TRAPDOOR, Material.DARK_OAK_TRAPDOOR,
+            Material.CRIMSON_TRAPDOOR, Material.WARPED_TRAPDOOR, Material.IRON_TRAPDOOR,
+            Material.OAK_FENCE_GATE, Material.SPRUCE_FENCE_GATE, Material.BIRCH_FENCE_GATE,
+            Material.JUNGLE_FENCE_GATE, Material.ACACIA_FENCE_GATE, Material.DARK_OAK_FENCE_GATE,
+            Material.CRIMSON_FENCE_GATE, Material.WARPED_FENCE_GATE,
+            Material.STONE_BUTTON, Material.OAK_BUTTON, Material.LEVER
+        )
+
+        if (block.type in blockedBlocks) {
+            event.isCancelled = true
+            PlayerMessage().send("<red><bold>You cannot interact with blocks in this server", player)
+        }
     }
 }
